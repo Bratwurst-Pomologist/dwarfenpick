881e0258e72e3c336aaf1ac209d66fb565a3edf6
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-02-12 13:27:16 +0100

    update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
 
+minetest.register_tool(":dwarfen:pick", {
+        description = "Dwarfen Pickaxe",
+        inventory_image = "default_tool_mesepick.png^default_obsidian_shard.png",
+        range = 11,
+        groups = {not_in_creative_inventory = 1},
+        tool_capabilities = {
+                full_punch_interval = 0.1,
+                max_drop_level = 3,
+                groupcaps = {
+                        unbreakable =   {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        dig_immediate = {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        fleshy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        choppy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        bendy =                {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        cracky =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        crumbly =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                        snappy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3}
+                },
+                damage_groups = {fleshy = 1000}
+        }
+})
