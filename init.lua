94d36e645030ee848773452f6ddbe803b79349f7
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-02-13 12:08:34 +0100

    update init.lua

--- init.lua
+++ init.lua
@@ -2,30 +2,31 @@
 minetest.register_tool(":dwarfen:pick", {
      description = "Dwarfen Pickaxe",
      inventory_image = "moreores_tool_mithrilpick.png^default_obsidian_shard.png",
-      range = 11,
+      range = 8,
       groups = {not_in_creative_inventory = 0},
       tool_capabilities = {
         full_punch_interval = 0.1,
         max_drop_level = 3,
-        groupcaps = {
-            unbreakable =   {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            dig_immediate = {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            fleshy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            choppy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            bendy =                {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            cracky =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            crumbly =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
-            snappy =        {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3}
-            },
-            damage_groups = {fleshy = 1000}
-}
+        groupcaps={cracky = {times={[1]=2.4, [2]=1.2, [3]=0.60}, uses=20, maxlevel=3},
+		    },
+		    damage_groups = {fleshy=5},
+	      },
 })
 
 minetest.register_tool(":dwarfen:lavapick", {
     description = "Dwarfen Lava Pickaxe",
     inventory_image = "moreores_tool_mithrilpick.png^default_obsidian_shard.png",
-    range = 11,
-    groups = {not_in_creative_inventory = 0},
+    range = 8,
+    groups = {not_in_creative_inventory = 0}
+    tool_capabilities = {
+        full_punch_interval = 0.1,
+        max_drop_level = 3,
+        groupcaps={cracky = {times={[1]=2.4, [2]=1.2, [3]=0.60}, uses=20, maxlevel=3},
+		    },
+		    damage_groups = {fleshy=5},
+	      },
+})
+
 
 minetest.register_craft({
     output = ":dwarfen:pick",
